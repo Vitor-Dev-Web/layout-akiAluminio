22bb667161a0d68e554467c14eb7ca5de55327f6
Vitor Kauã <vitor.kauan1@icloud.com>
2024-08-02 10:26:50 -0300

    Add files via upload

---
+++ aki-aluminio-site/estilo/js/index.js
@@ -0,0 +1,8 @@
+alert("Boas compras!")
+
+function darkTheme() {
+    document.body.style.backgroundColor = "black";
+}
+function whiteTheme() {
+    document.body.style.backgroundColor = "white";
+}
